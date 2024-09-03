dfbdb53e6a177d733b4998ef91330ae42419344b
Polar <147011322+PolarT-py@users.noreply.github.com>
2024-09-03 08:03:24 -0400

    e

---
+++ .config/scripts/cursor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+hyprctl setcursor 'Bibata-Modern-Ice' 24
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+gsettings set org.gnome.desktop.interface cursor-size '24'
