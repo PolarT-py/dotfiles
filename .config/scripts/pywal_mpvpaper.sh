9ff40d9ecc69f6ad222cf196d5a3ac6fbaefe413
Polar <147011322+PolarT-py@users.noreply.github.com>
2024-09-07 22:11:24 -0400

    new script for mpvpaper

---
+++ .config/scripts/pywal_mpvpaper.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# THIS CODE IS SO POORLY MADE I DECLARE THIS AS 
+
+echo "make sure that you currently have mpvpaper running"
+
+# Create image cache folder
+mkdir -p ~/.cache/pywal_vid_frames/
+
+# Get video path
+file=$(echo '{ "command": ["get_property", "path"] }' | socat - /tmp/mpv-socket | jq -r '.data')
+
+echo "$file"
+
+# Check if success
+if [ -z "$file" ]; then
+    echo "Error: Could not retrieve video path. (You might need the packages 'jq' and 'socat')"
+    exit 1
+fi
+
+# get image stuff
+output_folder="$HOME/.cache/pywal_vid_frames/"
+base_name=$(basename "$file" .mp4)
+image=$(basename "$file" .png)
+
+echo "IIIIIIIIIMMMMMMMMMAGEEE ${output_folder}${image}"
+echo "${image}.png"
+echo "${output_folder}"
+
+ffmpeg -y -i "$file" -vf "select=eq(n\,59)" -frames:v 1 "${image}.png"
+mv "${image}.png" "$output_folder"
+
+echo "EEEEEE EEEEEEEEEEE EEEEEE ^&(*&*&(*&**(&*(&(*&(*&(* $(ls ~/.cache/pywal_vid_frames/)"
+
+# Check if success x2
+if [ -z "$image" ]; then
+    echo "Error: Could not retrieve image. (you might need the package 'ffmpeg')"
+fi
+
+
+# Set colors
+echo "IMAGE PATH:::::: ${output_folder}${image}.png"
+wal -i "${output_folder}${image}.png"
+
+killall waybar && waybar &
