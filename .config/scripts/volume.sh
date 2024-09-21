816e410cb69844cf9f9a2b0979d91e51d33e41be
Polar <147011322+PolarT-py@users.noreply.github.com>
2024-09-21 15:15:51 -0400

    hmhmm volume.sh changeds

--- .config/scripts/volume.sh
+++ .config/scripts/volume.sh
@@ -3,7 +3,10 @@
 # Get the action
 action=$1
 
-if [ "$action" == "up" ]; then
+volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%' | head -n 1)
+echo "${volume%?}"
+
+if [ "$action" == "up" -a "${volume%?}" -le "95" ]; then
     pactl set-sink-volume @DEFAULT_SINK@ +5%
 elif [ "$action" == "down" ]; then
     pactl set-sink-volume @DEFAULT_SINK@ -5%
@@ -11,11 +14,12 @@ elif [ "$action" == "mute" ]; then
     pactl set-sink-mute @DEFAULT_SINK@ toggle
 fi
 
+# IDK HOW TO SUBTRACT 5 FROM vOLUME
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | grep -o '[0-9]*%' | head -n 1)
 mute=$(pactl get-sink-mute @DEFAULT_SINK@ | grep -o 'yes\|no')
 
 if [ "$mute" == "yes" ]; then
-    dunstify -u low -i ~/.local/share/icons/hicolor/32x32/apps/CustomIcons/volume_mute.svg -h string:x-dunst-stack-tag:volume "Muted" "${volume%?}%" 
+    dunstify -u low -i ~/.config/dunst/images/shootmyself.png -h string:x-dunst-stack-tag:volume "Volume Muted" "${volume%?}%" -h int:value:${volume} 
 else
     if [ "${action}" == "up" ]; then
         dunstify -u low -h string:category:volume -i ~/.config/dunst/images/shootmyself.png -h string:x-dunst-stack-tag:volume "Volume" "${volume%?}%" -h int:value:${volume}
