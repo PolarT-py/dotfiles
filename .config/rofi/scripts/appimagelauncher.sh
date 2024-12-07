892120d35ff860557f05fa525e06a801aa847d5d
Polar <147011322+PolarT-py@users.noreply.github.com>
2024-12-07 13:44:15 -0500

    new rofi stuff from https://github.com/adi1090x/rofi\?tab\=readme-ov-file

--- .config/rofi/scripts/appimagelauncher.sh
+++ .config/rofi/scripts/appimagelauncher.sh
@@ -4,7 +4,7 @@
 DIR="$HOME/Desktop/appimages/"
 
 # Show all
-SELECTED=$(ls "$DIR" | rofi -dmenu -p "Select app" -i)
+SELECTED=$(ls "$DIR" | rofi -dmenu -p "Select app" -i -theme $HOME/.config/rofi/launchers/type-1/style-7.rasi)
 
 # Select and run file
 if [ -n "$SELECTED" ]; then
