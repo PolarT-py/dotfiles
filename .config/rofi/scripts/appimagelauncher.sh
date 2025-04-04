0edae909d5c88876145d5893b3afd59f04fe3af2
Polar <147011322+PolarT-py@users.noreply.github.com>
2025-04-04 18:07:52 -0400

    a

--- .config/rofi/scripts/appimagelauncher.sh
+++ .config/rofi/scripts/appimagelauncher.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Directory to list
+# Directory to search
 DIR="$HOME/Desktop/appimages/"
 
-# Show all
-SELECTED=$(ls "$DIR" | rofi -dmenu -p "Select app" -i -theme $HOME/.config/rofi/launchers/type-1/style-7.rasi)
+# Find all executable files in the directory and subdirectories
+mapfile -t FILES < <(find "$DIR" -type f -executable)
 
-# Select and run file
-if [ -n "$SELECTED" ]; then
-    ~/Desktop/appimages/"$SELECTED"
-    #notify-send e ~/"$SELECTED"
+# Show only filenames in rofi, preserving spaces
+SELECTED_NAME=$(printf "%s\n" "${FILES[@]##*/}" | rofi -dmenu -p "Select app" -i -theme "$HOME/.config/rofi/launchers/type-1/style-7.rasi")
+
+# Find full path corresponding to the selected name
+if [ -n "$SELECTED_NAME" ]; then
+    for file in "${FILES[@]}"; do
+        if [[ "$(basename "$file")" == "$SELECTED_NAME" ]]; then
+            "$file" &
+            break
+        fi
+    done
 fi
