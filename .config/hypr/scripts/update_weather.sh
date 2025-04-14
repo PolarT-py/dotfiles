8e21c2a74306c08b4c5c5e71b904a33910c28ab3
Polar <147011322+PolarT-py@users.noreply.github.com>
2025-04-13 20:32:34 -0400

    sigma

--- .config/hypr/scripts/update_weather.sh
+++ .config/hypr/scripts/update_weather.sh
@@ -2,6 +2,6 @@
 
 touch ~/.cache/pt_weather
 
-weather=$(wget -qO- "https://api.weatherapi.com/v1/current.json?key=fa7f85cdc69d4c75884222250242110&q=London,Canada" | jq -r '"\(.location.name) \(.current.temp_c)°C \(.current.condition.text)"')
+weather=$(wget -qO- "https://api.weatherapi.com/v1/current.json?key=fa7f85cdc69d4c75884222250242110&q=Toronto,Canada" | jq -r '"\(.location.name) \(.current.temp_c)°C \(.current.condition.text)"')
 
 echo $weather > ~/.cache/pt_weather
